ab615b62d6db506ba9d000d9d4796db6ce8b0021
sarahsj-dot <sarah.sdejesusp@gmail>
2025-02-24 11:09:01 -0300

    Exercicios de if else

--- src/aula3/exercicio2.java
+++ src/aula3/exercicio2.java
@@ -1,10 +1,27 @@
 package aula3;
 
+import java.util.Scanner;
+
 public class exercicio2 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Exercicio 2
+		
+		Scanner sc = new Scanner(System.in);
+		double larguraQ = 0, alturaQ = 0, area = 0, pe, diagonalQ = 0; 
+		
+		System.out.printf("Digite a largura do quadrado: %.1f", larguraQ);
+		larguraQ = sc.nextDouble();
+		System.out.printf("Digite a altura do quadrado: %.1f", alturaQ);
+		alturaQ = sc.nextDouble();
+		area = larguraQ + alturaQ;
+		pe = larguraQ *2 + alturaQ *2;
+		diagonalQ = Math.pow(larguraQ * larguraQ, diagonalQ);
+		System.out.printf("Área = %.4f", area);
+		System.out.printf("Perímetro = %.4f", pe);
+		System.out.printf("Diagonal: %.4f", diagonalQ);
+		
+		sc.close();
+	
 	}
-
 }
